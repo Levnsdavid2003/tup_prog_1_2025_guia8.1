658e9ffaf3a7100da704ef4935ff632b3acb46cf
Levnsdavid2003 <levinezdavid@gmail.com>
2025-06-11 21:38:10 -0300

    agregado ejercicio2

---
+++ guia8.1/ejercicio2/Models/Servicio.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2.Models
+{
+    internal class Servicio
+    {
+        #region Declaración de Variables
+        public int edad0, edad1, edad2, edad3;
+        public double monto;
+        public double porcentaje0, porcentaje1, porcentaje2, porcentaje3;
+        public double monto0, monto1, monto2, monto3;
+        #endregion
+
+        #region Resolución del Problema
+
+        public void RegistrarMontoARepartir(double valor)
+        {
+            monto = valor;
+        }
+        public void RegistrarEdad(int edad, int nroNiña)
+        {
+            switch (nroNiña)
+            {
+                case 0:
+                    edad0 = edad;
+                    break;
+                case 1:
+                    edad1 = edad;
+                    break;
+                case 2:
+                    edad2 = edad;
+                    break;
+                case 3:
+                    edad3 = edad;
+                    break;
+            }
+        }
+        public void CalcularMontosYPorcentajesARepartir()
+        {
+            int sumaEdades = edad0 + edad1 + edad2 + edad3;
+            porcentaje0 = (double)edad0 / sumaEdades * 100;
+            porcentaje1 = (double)edad1 / sumaEdades * 100;
+            porcentaje2 = (double)edad2 / sumaEdades * 100;
+            porcentaje3 = (double)edad3 / sumaEdades * 100;
+            monto0 = monto * porcentaje0 / 100;
+            monto1 = monto * porcentaje1 / 100;
+            monto2 = monto * porcentaje2 / 100;
+            monto3 = monto * porcentaje3 / 100;
+        }
+        #endregion
+    }
+}
---
+++ guia8.1/ejercicio2/Program.cs
@@ -0,0 +1,102 @@
+﻿using ejercicio2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    internal class Program
+    {
+        static Servicio servicio = new Servicio();
+
+
+        #region Metodos de Pantalla
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine(
+                @"Procesar mensualidad de Niñas
+
+        1. Iniciar Monto a repartir
+        2. Solicitar Edad por Niña
+        3. Mostrar monto y porcentajes que corresponde a cada niña.");
+            int eleccion = Convert.ToInt32(Console.ReadLine());
+
+            return eleccion;
+        }
+        static void MostrarPantallaSolicitarMontoARepartir()
+        {
+            Console.Clear();
+            Console.WriteLine("Monto a repartir entre las niñas\n");
+            Console.WriteLine("Ingrese el monto a repartir entre las niñas:");
+            double monto = Convert.ToDouble(Console.ReadLine());
+            servicio.RegistrarMontoARepartir(monto);
+            Console.WriteLine("\n\nPresione cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+        static void MostrarPantallaSolicitarEdadesNiñas()
+        {
+            Console.Clear();
+            Console.WriteLine("Edades de niñas:\n");
+            Console.WriteLine("Ingrese la edad de cada niña\n");
+            Console.WriteLine("Niña 1:");
+            int edad = Convert.ToInt32(Console.ReadLine());
+            servicio.RegistrarEdad(edad, 0);
+            Console.WriteLine("Niña 2:");
+            edad = Convert.ToInt32(Console.ReadLine());
+            servicio.RegistrarEdad(edad, 1);
+            Console.WriteLine("Niña 3:");
+            edad = Convert.ToInt32(Console.ReadLine());
+            servicio.RegistrarEdad(edad, 2);
+            Console.WriteLine("Niña 4:");
+            edad = Convert.ToInt32(Console.ReadLine());
+            servicio.RegistrarEdad(edad, 3);
+
+            Console.WriteLine("\n\nPresione cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+        static void MostrarPantallaCalcularMostrarMontoYPorcentajePorNiña()
+        {
+            Console.Clear();
+            servicio.CalcularMontosYPorcentajesARepartir();
+            Console.WriteLine("Monto y porcentajes que corresponde a cada niña:\n");
+            Console.WriteLine($"Niña 1: {servicio.monto0:f2}$ ({servicio.porcentaje0:f2}%)");
+            Console.WriteLine($"Niña 2: {servicio.monto1:f2}$ ({servicio.porcentaje1:f2}%)");
+            Console.WriteLine($"Niña 3: {servicio.monto2:f2}$ ({servicio.porcentaje2:f2}%)");
+            Console.WriteLine($"Niña 4: {servicio.monto3:f2}$ ({servicio.porcentaje3:f2}%)");
+            Console.WriteLine("\n\nPresione cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+        #endregion
+
+
+        static void Main(string[] args)
+        {
+            int op = MostrarPantallaSolicitarOpcionMenu();
+
+            while (op != -1)
+            {
+                switch (op)
+                {
+                    case 1:
+                        MostrarPantallaSolicitarMontoARepartir();
+                        break;
+                    case 2:
+                        MostrarPantallaSolicitarEdadesNiñas();
+                        break;
+                    case 3:
+                        MostrarPantallaCalcularMostrarMontoYPorcentajePorNiña();
+                        break;
+                    default:
+                        op = -1;
+                        break;
+
+                }
+                op = MostrarPantallaSolicitarOpcionMenu();
+            }
+        }
+    }
+}
